74e7196598612d54aef0b911303a08ea703450fc
Federico Ceratto <federico.ceratto@gmail.com>
2010-11-13 15:16:25 +0000

    testloop.sh removed

--- testloop.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-while true
-do
-    inotifywait -e MOVE_SELF *.py test/tests.py 2>/dev/null
-    echo -e '\n======================================================================\n'
-    nosetests test/tests.py
-done
