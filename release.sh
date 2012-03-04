8dbd624017f421007c0821511b9c19c52983f4f7
Federico Ceratto <federico.ceratto@gmail.com>
2012-03-04 11:59:17 +0000

    release script improved

--- release.sh
+++ release.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
-set -x
+set -e
 set -u
 
 PACKAGING_DIR=~/projects/packaging/firelet
 PACKAGING_OUT_DIR=~/projects/packaging/build-area
 
-# Extract version
+function ask() {
+    echo -ne "\n$1 [y/n]? "; read ANS
+    [[ $ANS == "y" ]] && return 0;
+    return 1;
+}
 
+# Extract version
 V=$(python -c 'from firelet.flcore import __version__ as v; print v')
 
-echo "Press enter to release v. $V"
-read
+if ! ask "release v. $V"; then exit; fi
 
 # Based on the __version__ value in flcore.py , build:
-
 #tar.gz source:
 python setup.py sdist
 
@@ -23,22 +26,20 @@ python setup.py sdist
 python setup.py bdist --formats=gztar,rpm
 
 # Debian packaging
-cp dist/firelet-"$V".tar.gz ../packaging/tarballs/firelet_"$V".orig.tar.gz
-ODIR=$(pwd)
-cd $PACKAGING_DIR
-git-import-orig ../tarballs/firelet_"$V".orig.tar.gz
-dch -v $V-1 -r experimental 'New upstream release'
-git commit -a -m "New upstream release: $V"
-git-buildpackage --git-ignore-new
+if ask "Perform Debian packaging"; then
+    cp dist/firelet-"$V".tar.gz ../packaging/tarballs/firelet_"$V".orig.tar.gz
+    ODIR=$(pwd)
+    cd $PACKAGING_DIR
+    git-import-orig ../tarballs/firelet_"$V".orig.tar.gz
+    dch -v $V-1 --distribution experimental 'New upstream release'
+    git commit -a -m "New upstream release: $V"
+    git-buildpackage --git-ignore-new
 
-echo "Install .deb package locally [y/n]?"
-read ANS
-if ["$ANS" == 'y'] then
-    cd $PACKAGING_OUT_DIR
-    sudo dpkg -i firelet_$V-1_all.deb
+    if ask "Install .deb package locally"; then
+        cd $PACKAGING_OUT_DIR
+        sudo dpkg -i firelet_$V-1_all.deb
+    fi
 fi
-
-cd $ODIR
 
 
 
