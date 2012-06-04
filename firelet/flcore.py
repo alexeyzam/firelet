e443aa944c2582d96df03658dbd53e553325417b
Federico Ceratto <federico.ceratto@gmail.com>
2012-06-04 16:14:52 +0100

    Version bumped to 0.5.0a1

--- firelet/flcore.py
+++ firelet/flcore.py
@@ -31,7 +31,7 @@ from time import time
 from flssh import SSHConnector, MockSSHConnector
 from flutils import Alert, Bunch, flag, extract_all
 
-__version__ = '0.4.5a2'
+__version__ = '0.5.0a1'
 
 from logging import getLogger
 log = getLogger(__name__)
